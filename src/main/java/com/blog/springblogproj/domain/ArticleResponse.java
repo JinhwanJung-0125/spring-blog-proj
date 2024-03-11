18984afde929f5b91bd1c0aa459a123a7f8fd011
JinhwanJung-0125 <joung.jinhawn@gmail.com>
2024-03-11 18:10:21 +0900

    Feat : ArticleResponse 객체 구성

---
+++ src/main/java/com/blog/springblogproj/domain/ArticleResponse.java
@@ -0,0 +1,18 @@
+package com.blog.springblogproj.domain;
+
+import lombok.Getter;
+
+@Getter
+public class ArticleResponse {
+    private final String title;
+    private final String content;
+
+    private ArticleResponse(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    public static ArticleResponse convert(Article article) {
+        return new ArticleResponse(article.getTitle(), article.getContent());
+    }
+}
