d1e6670e2553c774abac208dd4c5d8d64e477440
JinhwanJung-0125 <joung.jinhawn@gmail.com>
2024-03-11 11:03:11 +0900

    Initial commit

---
+++ src/test/java/com/blog/springblogproj/SpringBlogProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blog.springblogproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBlogProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
